2e3f36889568ea04a6d21f9eb28f361bc6879744
ab2101198 <116869072+ab2101198@users.noreply.github.com>
2024-12-23 19:46:27 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,7 +27,7 @@ app.use(
     saveUninitialized: false,
     resave: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24, 
-              secure: process.env.NODE_ENV === 'production',  // Ensure cookie is only sent over HTTPS
+              secure: true,  // Ensure cookie is only sent over HTTPS
               // sameSite: 'strict',  // Allow cookie to be sent with cross-origin requests
               // httpOnly: true,
             },
